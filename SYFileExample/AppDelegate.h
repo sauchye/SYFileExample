d9c18fd56f64a025ebdba757556909fee0dcb7bd
Sauchye <sauchyer@gmail.com>
2015-10-30 23:34:56 +0800

    Initial Commit

---
+++ SYFileExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SYFileExample
+//
+//  Created by Saucheong Ye on 10/30/15.
+//  Copyright © 2015 Saucheong Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SYFileExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SYFileExample
+//
+//  Created by Saucheong Ye on 10/30/15.
+//  Copyright © 2015 Saucheong Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
